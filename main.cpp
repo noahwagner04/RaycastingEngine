bd5ccb85ad22c08ed15266f2c760adc2a1c81838
Noah Wagner <wagnernoah@icloud.com>
2021-03-14 10:45:15 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "quickcg.h"
+using namespace QuickCG;
+using namespace std;
+
+int main(int, char *[])
+{
+	return 0;
+}
